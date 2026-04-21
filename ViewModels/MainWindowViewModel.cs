81e60ab266d4d09c7175844b427a506111e63093
Odlanyer <escolar.odlanyer@gmail.com>
2026-04-20 19:38:00 -0700

    Aprendí a usar enlace de datos y a llamarlos desde axaml... Funcionó perfectamente 😂

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace Cjng.ViewModels;
+﻿using System;
+namespace Cjng.ViewModels;
+
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Hello Avalonia!";
+    public string FechaHoy { get; } = "hoy es " + DateTime.Now.ToString("dd/MM/yyyy");
 }
