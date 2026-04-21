69f5d4d3a121fdad515b7461a942ed2af3eb411a
Odlanyer <escolar.odlanyer@gmail.com>
2026-04-20 19:11:44 -0700

    Hoy Termine la interfaz de la pantalla de entrada.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Cjng.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Hello Avalonia!";
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Cjng.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TextBox_SizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+    }
+}
